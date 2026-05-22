fa54409efe60c5bca9690a8a58a85d76fe1b04ee
gitneko <67227083+gitneko@users.noreply.github.com>
2026-05-22 21:41:20 +0200

    Update profile picture URL to a lower resolution
    HTTP 400 returned as-is, this fixes the issue as Wikimedia seems to force the thumbnail sizes.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -106,7 +106,7 @@
                 <div class="flex flex-row gap-4 items-center p-4">
                     <img
                         class="w-16 rounded-full"
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/1920px-Windows_10_Default_Profile_Picture.svg.png"
                         alt="Profile"
                     />
                     <div>
